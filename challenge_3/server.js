c47586f50918a8a0c280ce9cb29915378305b89b
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-14 19:43:08 -0800

    Server now can serve up the static files

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,6 @@
-
+var express = require('express');
+var app = express();
+app.use(express.static('client'));
+app.use('/node_modules', express.static('node_modules'));
+app.listen(3000, () => {console.log('listening on 3k :O)');});
 
